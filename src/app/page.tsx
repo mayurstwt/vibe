41967c4fdbf30bfad78f53318c74556af2b1cd95
mayurstwt <mayurstwt@.com>
2025-07-27 21:20:47 +0530

    05-ai-jobs file added again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
 import { toast } from "sonner"
 
 const Page = () => {
+   const [value, setValue] = useState("")
   const trpc = useTRPC()
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
@@ -16,9 +19,12 @@ const Page = () => {
     }
   }))
   return (
-    <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Mayur" })}>
-      Invoke Background job
-    </Button>
+        <div className="flex flex-col items-center justify-center h-screen max-w-md mx-auto gap-y-4">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background job
+      </Button>
+    </div>
   )
 }
 
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,22 @@
 import { inngest } from "./client";
+import { createAgent, gemini } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer and AI coding assistant. You write clean, efficient, and idiomatic React and TypeScript code using best practices. You provide clear explanations, concise code samples, and helpful suggestions. When asked for code, you ensure it is production-ready and follows modern Next.js conventions. If you need more information, you ask clarifying questions before proceeding. Here you don't have to ask for additional information, you just write the code.",
+      model: gemini({ model: "gemini-2.0-flash-lite" }),
+    });
 
-    await step.sleep("wait-a-moment", "7s");
+    const { output } = await codeAgent.run(event.data.value);
+    // console.log(output);
 
-    await step.sleep("wait-a-moment", "5s");
-
-    return { message: `Hello ${event.data.email}!` };
-  },
+    return {
+      output,
+    };
+  }
 );
