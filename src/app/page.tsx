b2a1bbb94f842bf1bc9877a5d926b2373530421e
mayurstwt <mayurstwt@.com>
2025-08-03 23:55:07 +0530

    08-messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,41 @@
 
 import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { toast } from "sonner"
 import { Input } from "@/components/ui/input"
 import { useState } from "react"
 
 const Page = () => {
-
   const [value, setValue] = useState("")
 
   const trpc = useTRPC()
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const {data: messages} = useQuery(trpc.messages.getmany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
-      toast.success("Background job invoked")
+      toast.success("Message Created")
     },
-    onError: () => {
+    onError: (error) => {
+      console.error(error)
       toast.error("Failed to invoke background job")
     }
   }))
   return (
-    <div className="bg-black">
-    <div className="flex flex-col items-center justify-center h-screen max-w-md mx-auto gap-y-4">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} className="text-white"/>
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
-        Invoke Background job
-      </Button>
-    </div>
+    <div className="bg-black text-white">
+      <div className="flex flex-col items-center justify-center min-h-screen max-w-md mx-auto gap-y-4">
+        <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="text-white"
+        />
+        <Button
+          disabled={createMessage.isPending}
+          onClick={() => createMessage.mutate({ value: value })}
+        >
+          Invoke Background job
+        </Button>
+        {JSON.stringify(messages, null, 2)}
+      </div>
     </div>
   )
 }
